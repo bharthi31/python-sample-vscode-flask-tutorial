b2b212f919d3614b47737e4fa9c277269c6b0eb2
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-03-19 13:49:19 +0530

    Update resources.tf

--- blue-green-tf/resources.tf
+++ blue-green-tf/resources.tf
@@ -33,7 +33,7 @@ resource "citrixadc_service" "blue_service" {
 resource "citrixadc_service" "green_service" {
     lbvserver = citrixadc_lbvserver.greenLB.name
     name = "green_service"
-    ip = "10.0.0.4"
+    ip = "172.16.5.5"
     servicetype  = "HTTP"
     port = 80
 }
@@ -44,7 +44,7 @@ resource "citrixadc_cspolicy" "blue_cspolicy" {
   csvserver       = citrixadc_csvserver.canary_csvserver.name
   targetlbvserver = citrixadc_lbvserver.blueLB.name
   policyname      = "blue_policy"
-  rule            = "sys.random.mul(100).lt(60)"
+  rule            = "sys.random.mul(100).lt(100)"
   priority        = 100
 
   # Any change in the following id set will force recreation of the cs policy
@@ -58,7 +58,7 @@ resource "citrixadc_cspolicy" "green_cspolicy" {
   csvserver       = citrixadc_csvserver.canary_csvserver.name
   targetlbvserver = citrixadc_lbvserver.greenLB.name
   policyname      = "green_policy"
-  rule            = "sys.random.mul(100).lt(40)"
+  rule            = "sys.random.mul(100).lt(0)"
   priority        = 101
 
   # Any change in the following id set will force recreation of the cs policy
