eb73399b68c309322f5781dee3e15d2a9eba4fd3
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-03-19 14:51:57 +0530

    Create resources.tf

---
+++ blue-green-tf/green-tf/resources.tf
@@ -0,0 +1,38 @@
+#cs_lb --Connecting to CSVServer to both LB Servers
+resource "citrixadc_csvserver" "demo_csvserver" {
+  ipv46       = "172.17.0.5"
+  name        = "demo_csvserver"
+  port        = 80
+  servicetype = "HTTP"
+
+  lbvserverbinding = citrixadc_lbvserver.greenLB.name
+}
+
+resource "citrixadc_lbvserver" "greenLB" {
+  ipv46       = "1.1.1.2"
+  name        = "greenLB"
+  port        = 80
+  servicetype = "HTTP"
+}
+
+resource "citrixadc_service" "green_service" {
+    lbvserver = citrixadc_lbvserver.greenLB.name
+    name = "green_service"
+    ip = "172.16.5.5"
+    servicetype  = "HTTP"
+    port = 80
+}
+
+resource "citrixadc_cspolicy" "green_cspolicy" {
+  csvserver       = citrixadc_csvserver.demo_csvserver.name
+  targetlbvserver = citrixadc_lbvserver.greenLB.name
+  policyname      = "green_policy"
+  rule            = "sys.random.mul(100).lt(40)"
+  priority        = 101
+
+  # Any change in the following id set will force recreation of the cs policy
+  forcenew_id_set = [
+    citrixadc_lbvserver.greenLB.id,
+    citrixadc_csvserver.demo_csvserver.id,
+  ]
+}
