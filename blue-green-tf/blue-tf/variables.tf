fb9ed4427ce908188053cadd4dc31166a5965804
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-04-01 18:20:53 +0530

    Update variables.tf

--- blue-green-tf/blue-tf/variables.tf
+++ blue-green-tf/blue-tf/variables.tf
@@ -2,10 +2,6 @@ variable "backend_service" {
   description = "The backend service IP"
   type        = string
 }
-variable "traffic_split_percentage" {
-  description = "Percentage of Traffic to be split"
-  type        = number
-}
 variable "username" {
   type        = string
 }
