4495f99a31104fd946c8cfb77da35a024966f1bb
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-05-28 20:59:21 +0530

    Update terraform_input_generator.py

--- blue-green-tf/green-tf/terraform_input_generator.py
+++ blue-green-tf/green-tf/terraform_input_generator.py
@@ -1,11 +1,12 @@
+import argparse
 import json
 import os
 
 
 class TerraformInputGenerator(object):
     @staticmethod
-    def populate_env():
-        with open("config.json") as json_file:
+    def populate_env(input_params):
+        with open(input_params.config_file) as json_file:
             json_data = json.load(json_file)
             for param, value in json_data.iteritems():
                 os.environ[param] = value
@@ -15,12 +16,20 @@ class TerraformInputGenerator(object):
                 print("{} = {}".format(param, os.environ.get(param, 'Oops!! Value not set')))
 
 
-def main():
-    TerraformInputGenerator.populate_env()
+def main(input_params):
+    TerraformInputGenerator.populate_env(input_params)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Terraform input generator')
+    parser.add_argument('--config_file', nargs='?', default=None,
+                        help='Config.json path')
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     try:
-        main()
+        arguments = parse_arguments()
+        main(arguments)
     except Exception as e:
         print('Exception occurred with reason: {}'.format(e))
