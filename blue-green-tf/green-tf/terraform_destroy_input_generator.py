0a89a40ea31cf0cb4a8734b90edd262e3217edd9
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-06-07 20:20:53 +0530

    Create terraform_destroy_input_generator.py

---
+++ blue-green-tf/green-tf/terraform_destroy_input_generator.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+
+
+class TerraformDestroyInputGenerator(object):
+    @staticmethod
+    def populate_env(input_params):
+        with open(input_params.config_file) as json_file:
+            json_data = json.load(json_file)
+            print('##vso[task.setvariable variable=TEARDOWN_FLAG]True')
+            for param, value in json_data.iteritems():
+                print('##vso[task.setvariable variable=TEARDOWN_{}]{}'.format(param, value))
+
+
+def main(input_params):
+    TerraformDestroyInputGenerator.populate_env(input_params)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Terraform Destroy input generator')
+    parser.add_argument('--config_file', nargs='?', default=None,
+                        help='config.json path')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    try:
+        arguments = parse_arguments()
+        main(arguments)
+    except Exception as e:
+        print('Exception occurred with reason: {}'.format(e))
