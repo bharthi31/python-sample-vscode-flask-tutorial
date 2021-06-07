df12589b7b4148b789abca46b7217d8e014c8032
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-06-07 22:10:27 +0530

    Update provider.tf

--- blue-green-tf/blue-tf/provider.tf
+++ blue-green-tf/blue-tf/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    citrixadc = {
+      source = "citrix/citrixadc"
+      version = "1.0.1"
+    }
+  }
+}
+
 provider "citrixadc" {
   endpoint = "http://20.0.0.4"
   username = var.username
