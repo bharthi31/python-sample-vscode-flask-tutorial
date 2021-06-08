aadb73b43d31a1f641feae0bca10c2144e814814
bharthi31 <73241453+bharthi31@users.noreply.github.com>
2021-06-08 13:24:42 +0530

    Update variables.tf

--- blue-green-tf/blue-tf/variables.tf
+++ blue-green-tf/blue-tf/variables.tf
@@ -6,9 +6,3 @@ variable "backend_service" {
   description = "The backend service IP"
   type        = string
 }
-variable "username" {
-  type        = string
-}
-variable "password" {
-  type        = string
-}
