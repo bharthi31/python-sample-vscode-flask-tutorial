94bfcf27a3a0b36880d2a62be5d41be9b47c1f49
Bharthi <bharthi31@mgail.co>
2021-06-14 17:20:36 +0530

    maintaining same script to teardown process

--- blue-green-tf/azure_pipelines/config/read_config.py
+++ blue-green-tf/azure_pipelines/config/read_config.py
@@ -5,10 +5,13 @@ import json
 class TerraformInputGenerator(object):
     @staticmethod
     def populate_env(input_params):
+        if input_params.action == 'delete':
+            print('##vso[task.setvariable variable=TEARDOWN_FLAG]True')
         with open(input_params.config_file) as json_file:
             json_data = json.load(json_file)
-            for param, value in json_data.iteritems():
-                print('##vso[task.setvariable variable={}]{}'.format(param, value))
+            if json_data.keys():
+                for param, value in json_data.iteritems():
+                    print('##vso[task.setvariable variable=TEARDOWN_{}]{}'.format(param, value))
 
 
 def main(input_params):
@@ -19,6 +22,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Terraform input generator')
     parser.add_argument('--config_file', nargs='?', default=None,
                         help='config.json path')
+    parser.add_argument('--action', nargs='?', default=None,
+                        help='create/delete')
     return parser.parse_args()
 
 
